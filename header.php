b3942696004140cd376879fac9cdc55bc562c119
Anurag Meena <ianuragit@gmail.com>
2014-05-26 03:21:18 +0530

    header & menu update

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<center>
+<ul id="nav">
+	<li class="home"><a href="#">Home</a></li>
+	<li class="tutorials"><a href="#">Tracking</a></li>
+	<li class="about"><a href="#">Warehouse</a></li>
+	<li class="news"><a href="#">Report</a></li>
+	<li class="contact"><a href="#">Contact</a></li>
+</ul>
+</center>
+    
+    <br>
+    <br>
